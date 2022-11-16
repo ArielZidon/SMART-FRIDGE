a9f27e076d919229e2a24028787b2cc84f75ffa8
Tair Cohen <arielz742@gmail.com>
2022-11-16 14:35:30 +0200

    firebase

--- app/src/main/java/com/example/smartfridge/MainActivity.java
+++ app/src/main/java/com/example/smartfridge/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.smartfridge;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
@@ -12,11 +13,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.smartfridge.ui.main.SectionsPagerAdapter;
 import com.example.smartfridge.databinding.ActivityMainBinding;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
 
     private ActivityMainBinding binding;
 
@@ -26,6 +35,28 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        firestore = FirebaseFirestore.getInstance();
+
+        Map<String, Object> users = new HashMap<>();
+        users.put("ariel","backend");
+        users.put("eran","database");
+        users.put("ofir","administration + database");
+        users.put("afik","design");
+
+
+        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
+
+            }
+        }).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(),"Failue",Toast.LENGTH_LONG).show();
+            }
+        });
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = binding.viewPager;
