ad0362c6bd3d968f403f71c81f1ae97f3ca02bf1
afikdamri <afikdamri97@gmail.com>
2022-12-06 11:12:43 +0200

    finish

--- app/src/main/java/com/example/smartfridge/list_item.java
+++ app/src/main/java/com/example/smartfridge/list_item.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +20,7 @@ public class list_item extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    Button by;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
