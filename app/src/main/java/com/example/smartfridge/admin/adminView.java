b962137ec86467ca21c6551dbe77702daeb92687
ArielZidon <arielz742@gmail.com>
2022-12-27 17:10:33 +0200

    to ofir

---
+++ app/src/main/java/com/example/smartfridge/admin/adminView.java
@@ -0,0 +1,14 @@
+package com.example.smartfridge.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class adminView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_view);
+    }
+}
