b912cf0b296e42010a8f43a53aac7c87cd1fb864
EranTzarum <eran.davidtz@gmail.com>
2023-01-07 19:29:22 +0200

    insert_recipe.java Ingerd

--- app/src/main/java/com/example/smartfridge/recipesDB/insert_recipe.java
+++ app/src/main/java/com/example/smartfridge/recipesDB/insert_recipe.java
@@ -2,18 +2,19 @@ package com.example.smartfridge.recipesDB;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smartfridge.R;
+import com.example.smartfridge.ui.main.MainMenu;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -26,25 +27,62 @@ public class insert_recipe extends AppCompatActivity {
     LinearLayout layoutList;
     Button add;
     String ingredients = null;
-
-//    public insert_recipe(int image) {
-//        IMAGE = image;
-//    }
+    Button submit_list;
+    ArrayList<Ingredient> ingredients_List = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_recipe);
 
+        TextView recipe_name = (TextView) findViewById(R.id.recipe_name);
+        TextView recipe_time = (TextView) findViewById(R.id.recipe_time);
+        TextView recipe_instructions = (TextView) findViewById(R.id.recipe_instructions);
+
         layoutList = (LinearLayout) findViewById(R.id.layout_list);
         add = (Button) findViewById(R.id.button_add);
+        submit_list = (Button) findViewById(R.id.button_submit_list);
+
+
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 addView();
             }
         });
+
+
+
+        submit_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkIfValidAndRead();
+//                if(checkIfValidAndRead()){
+////                    Intent intent = new Intent(insert_recipe.this,ingredient_recycler.class);
+////                    Bundle bundle = new Bundle();
+////                    bundle.putSerializable("list",ingredients_List);
+////                    intent.putExtras(bundle);
+////                    startActivity(intent);
+//                }
+                for (int i = 0; i < ingredients_List.size(); i++) {
+                    insert_ingredients(ingredients_List.get(i).getIngredientName(),ingredients_List.get(i).getQuantity());
+                }
+                ArrayList<String> new_recipe = new ArrayList<>();
+                new_recipe.add(recipe_name.getText().toString());
+                new_recipe.add(recipe_time.getText().toString());
+                new_recipe.add(recipe_instructions.getText().toString());
+                new_recipe.add(ingredients);
+
+                Toast.makeText(insert_recipe.this,"Your RECIPE sent to admin!",Toast.LENGTH_LONG).show();
+
+                openMainMenu();
+
+                //****need to send to database;
+            }
+        });
+
 
         imgGallery = (ImageView) findViewById(R.id.img_gallery);
         Button open_gallery = (Button) findViewById(R.id.upload_image_bt);
@@ -55,16 +93,61 @@ public class insert_recipe extends AppCompatActivity {
                 Intent gallery = new Intent(Intent.ACTION_PICK);
                 gallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(gallery,IMAGE);
-
+                //***NEED INSERT IMAGE TO DATABASE
             }
         });
+    }
+
+    private boolean checkIfValidAndRead() {
+        ingredients_List.clear();
+        boolean result = true;
+
+        for (int i = 0; i < layoutList.getChildCount(); i++) {
+
+            View cricketerView = layoutList.getChildAt(i);
+
+            EditText ingred = (EditText)cricketerView.findViewById(R.id.edit_ingredient_name);
+            EditText quantity = (EditText)cricketerView.findViewById(R.id.quantity);
+
+            Ingredient Ingredient = new Ingredient();
+
+            if (!ingred.getText().toString().equals("")) {
+                Ingredient.setIngredientName(ingred.getText().toString());
+            }
+            else {
+                result = false;
+                break;
+            }
+
+            if (!quantity.getText().toString().equals("")) {
+                Ingredient.setQuantity(quantity.getText().toString());
+
+                ingredients_List.add(Ingredient);
+            }
+            else {
+                result = false;
+                break;
+            }
+
+
+            if (ingredients_List.size() == 0) {
+                result = false;
+                Toast.makeText(this, "Add Cricketers First!", Toast.LENGTH_SHORT).show();
+            }
+
+            else if(!result){
+                Toast.makeText(this, "Enter All Details Correctly!", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        return result;
     }
 
     private void addView(){
 
         final View cricketerView = getLayoutInflater().inflate(R.layout.activity_row_add_cricketer,null,false);
 
-        EditText ingredient = (EditText)cricketerView.findViewById(R.id.edit_ingredient_name);
+        EditText ingred = (EditText)cricketerView.findViewById(R.id.edit_ingredient_name);
         EditText quantity = (EditText)cricketerView.findViewById(R.id.quantity);
         ImageView imageClose = (ImageView)cricketerView.findViewById(R.id.image_remove);
         
@@ -78,15 +161,20 @@ public class insert_recipe extends AppCompatActivity {
         });
 
         layoutList.addView(cricketerView);
-//        insert_ingredient(ingredient,quantity);
 
     }
 
-    private void insert_ingredient(EditText ingredient,EditText quantity){
-
-        ingredients.concat(ingredient.getText().toString());
-        ingredients.concat(" " + quantity.getText().toString());
-        ingredients.concat("\n");
+    private void insert_ingredients(String ingredient,String quantity){
+        if (ingredients == null) {
+            ingredients = ingredient;
+            ingredients.concat(" " + quantity);
+            ingredients.concat("\n");
+        }
+        else {
+            ingredients.concat(ingredient);
+            ingredients.concat(" " + quantity);
+            ingredients.concat("\n");
+        }
     }
 
     private void removeView(View view){
@@ -106,5 +194,10 @@ public class insert_recipe extends AppCompatActivity {
             }
 
         }
+    }
+
+    public void openMainMenu(){
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
     }
 }
